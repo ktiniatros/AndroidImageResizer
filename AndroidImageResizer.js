67362405ae7bff29b68215a8fb31ba4271185c84
giorgos <giorgos@myPrecious.local>
2016-02-13 15:20:08 +0100

    copy files to android studio project automatically after resizing

--- AndroidImageResizer.js
+++ AndroidImageResizer.js
@@ -3,87 +3,127 @@ var path = require('path');
 var imagemagick = require('imagemagick');
 var readline = require('readline');
 
-var sizeNames = ['xxxhdpi', 'xxhdpi', 'xhdpi', 'hdpi', 'mdpi', 'ldpi'];
-var multipliers = [4.0, 3.0, 2.0, 1.5, 1, 0.75];
+//CHANGE THIS TO YOUR Android Studio path
+var androidStudioPath = "/Users/giorgos/Documents/androidstudio";
+var projectPath;
+var mipmapPath = "/app/src/main/res/mipmap-";
+
+var sizeNames = [/*'xxxhdpi', */'xxhdpi', 'xhdpi', 'hdpi', 'mdpi'/*, 'ldpi'*/];
+var multipliers = [/*4.0, */3.0, 2.0, 1.5, 1/*, 0.75*/];
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 var imageFiles = [];
 
-var currentImageSize;
+var currentImageSize = "xxhdpi";
 var smallestImageSize;
 
 function askUserForCurrentSize() {
-  rl.question("Enter current image size [xxxhdpi, xxhdpi, xhdpi, hdpi, mdpi, ldpi]: ",
-  function(inputString) {
-    currentImageSize = inputString;
-    askUserForSmallestSize();
-  });
+
+    // currentImageSize = 'xxhdpi';
+    // askUserForSmallestSize();
+    // return;
+    rl.question("Enter your project folder name: ",
+        function (inputString) {
+            projectPath = path.join(androidStudioPath, inputString);
+            if (!fs.existsSync(projectPath)) {
+                console.log(projectPath + " does not exist!");
+                askUserForCurrentSize();
+            } else {
+                androidStudioPath = projectPath;
+                askUserForSmallestSize();
+            }
+        });
 }
 
 function askUserForSmallestSize() {
-  rl.question("Enter smallest desired image size [xxxhdpi, xxhdpi, xhdpi, hdpi, mdpi, ldpi]: ",
-  function(inputString) {
-    smallestImageSize = inputString;
+    smallestImageSize = 'mdpi';
     makeDirectories();
-    rl.close();
-  });
+    return;
+    rl.question("Enter smallest desired image size [xxxhdpi, xxhdpi, xhdpi, hdpi, mdpi, ldpi]: ",
+        function (inputString) {
+            smallestImageSize = inputString;
+            makeDirectories();
+            rl.close();
+        });
 }
 
 function makeDirectories() {
-  console.log("Making directories...");
-  var startIndex = sizeNames.indexOf(currentImageSize);
-  var endIndex = sizeNames.indexOf(smallestImageSize);
-  for (var i = startIndex; i <= endIndex; i++) {
-    fs.mkdirSync(sizeNames[i]);
-  }
-  populateImageFiles();
+    // var startIndex = 0;//sizeNames.indexOf(currentImageSize);
+    // var endIndex = sizeNames.length - 1;//sizeNames.indexOf(smallestImageSize);
+    // for (var i = startIndex; i <= endIndex; i++) {
+    //     if (!fs.existsSync('./' + sizeNames[i])) {
+    //         console.log("Making directory: " + sizeNames[i]);
+    //         fs.mkdirSync(sizeNames[i]);
+    //     }
+    // }
+    populateImageFiles();
 }
 
 function populateImageFiles() {
-  console.log("Finding image files...");
-  var currentDirectory = process.cwd();
-  var allFiles = fs.readdirSync(currentDirectory);
-  for (var i = 0; i < allFiles.length; i++) {
-    var extname = path.extname(allFiles[i]);
-    if (extname == '.jpg' || extname == '.jpeg' || extname == '.png') {
-      imageFiles.push(allFiles[i]);
+    console.log("Finding image files...");
+    var currentDirectory = process.cwd();
+    var allFiles = fs.readdirSync(currentDirectory);
+    for (var i = 0; i < allFiles.length; i++) {
+        var extname = path.extname(allFiles[i]);
+        var f = allFiles[i];
+        console.log("extname", extname);
+        if (extname == '.jpg' || extname == '.jpeg' || extname == '.png') {
+            try {
+                fs.renameSync(f, f.replace('@3x', ''));
+            } catch (e) {
+                console.log("error removing @3x", e);
+            }
+            imageFiles.push(f.replace('@3x', ''));
+        }
     }
-    if (i == allFiles.length - 1) {
-      console.log("Resizing all images...");
-      resize(0, 0);
+    console.log("imageFiles", imageFiles);
+    console.log("allFiles", allFiles, i);
+    if (imageFiles.length > 0) {
+        console.log("Resizing all images...");
+        resize(0, 0);
+    } else {
+        console.log("no image files found (.jpg, .jpeg, .png)");
+        process.exit(); return;
     }
-  }
 }
 
 function resize(fileIndex, sizeIndex) {
-  imagemagick.convert(
-      [imageFiles[fileIndex], '-resize', getPercentString(sizeNames[sizeIndex]),
-      getPath(fileIndex, sizeIndex)],
-      function() {
-        if (sizeIndex < sizeNames.length) {
-          resize(fileIndex, sizeIndex + 1);
-        } else if (fileIndex < imageFiles.length) {
-          resize(fileIndex + 1, 0);
-        } else {
-          console.log("Done.")
-        }
-      });
+    var filePath = getPath(fileIndex, sizeIndex);
+    console.log("about to convert: " + imageFiles[fileIndex], fs.existsSync(imageFiles[fileIndex]));
+    imagemagick.convert(
+        [imageFiles[fileIndex], '-resize', getPercentString(sizeNames[sizeIndex]),
+            filePath],
+        function (e) {
+            if (e) {
+                console.log("error on resize for " + filePath, e);
+            }
+            if (sizeIndex + 1 < sizeNames.length) {
+                resize(fileIndex, sizeIndex + 1);
+            } else if ((fileIndex + 1) < imageFiles.length) {
+                resize(fileIndex + 1, 0);
+            } else {
+                console.log("Done.");
+                process.exit();
+            }
+        });
 }
 
 function getPath(fileIndex, sizeIndex) {
-  return sizeNames[sizeIndex] + '/' + imageFiles[fileIndex];
+    var finalPath = androidStudioPath + mipmapPath + sizeNames[sizeIndex] + "/" + imageFiles[fileIndex];
+    console.log("file to: " + finalPath);
+    return finalPath;
 }
 
 function getPercentString(sizeName) {
-  return getPercent(sizeName) * 100 + '%';
+    return getPercent(sizeName) * 100 + '%';
 }
 
 function getPercent(sizeName) {
-  return multipliers[sizeNames.indexOf(sizeName)] / multipliers[sizeNames.indexOf(currentImageSize)];
+    return multipliers[sizeNames.indexOf(sizeName)] / multipliers[sizeNames.indexOf(currentImageSize)];
 }
 
 askUserForCurrentSize();
--- README.md
+++ README.md
@@ -3,6 +3,12 @@ AndroidImageResizer
 
 A Node.js + ImageMagick script for resizing images for Android apps.
 
+Added by ktiniatros:
+If you are working with iOS apps too, you usually get the assets for iOS first. So, its handy to be able to generate the android assets from the @3x files(optional, this script works anyway with any file names as long as their extensions are jpg,jpeg or png).
+I went one step further and made this script to copy the generated files to the appropriate mipmap folders of your android project so you won't have to copy them manually everytime. Just change the androidStudioPath variable to your android studio workplace path. Everytime you need to generate assets, the script promts you to type the folder name of the project the assets are for.
+
+Questions/remarks/feedback: kwdikoi ατ gmail ντοτ com
+
 ## How to Use
 
 Install [ImageMagick for Node.js](https://github.com/rsms/node-imagemagick).
